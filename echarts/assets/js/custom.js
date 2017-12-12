3ed4e6b1856a73bfce5b0b0d0d89695277a00324
onlythesnowsnow <471841715@qq.com>
2017-12-12 22:35:06 +0800

    微博知乎

---
+++ echarts/assets/js/custom.js
@@ -0,0 +1,54 @@
+﻿
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+       
+        
+        gallery_fun: function () {
+            /*====================================
+                 FOR IMAGE/GALLERY POPUP
+            ======================================*/
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            /*====================================
+            FOR IMAGE/GALLERY FILTER
+            ======================================*/
+
+            // MixItUp plugin
+            // http://mixitup.io
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+      
+      
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.gallery_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
